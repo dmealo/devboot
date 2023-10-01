ce7171e87d017029ec1cc210dfa1be91f0033300
David Mealo <david.mealo@valtech.com>
2023-09-30 23:59:54 -0400

    Simplify devboot.ps1 to match general release of WinGet Configuration

--- devboot.ps1
+++ devboot.ps1
@@ -19,58 +19,57 @@ if (!(Test-Path $devbootPath\.vsconfig\VS2022)) {
     mkdir $devbootPath\.vsconfig\VS2022
 }
 
-# Get version number of latest preview version of winget
-$releases = Invoke-RestMethod -Uri https://api.github.com/repos/microsoft/winget-cli/releases
-$releases | ConvertTo-Json -Depth 100 | Out-File -FilePath .\releases.json
-$releases = Get-Content -Path .\releases.json -Raw | ConvertFrom-Json
-$latestPreviewRelease = $releases | Where-Object { $_.prerelease -eq $true } | Select-Object -First 1 | Select-Object -ExpandProperty tag_name
+# Get version number of latest release version of winget
+$latestRelease = Invoke-RestMethod -Uri https://api.github.com/repos/microsoft/winget-cli/releases | ConvertTo-Json -Depth 100 | ConvertFrom-Json | Where-Object { $_.prerelease -eq $false } | Select-Object -First 1 | Select-Object -ExpandProperty tag_name
 
 # Skip installation if latest already installed
 $wingetCurrentVersion = winget --version
-if ($wingetCurrentVersion -eq $latestPreviewRelease) {
-    Write-Host "Latest version of winget preview already installed (ignore following settings error prefixing version number display `
+if ($wingetCurrentVersion -eq $latestRelease) {
+    Write-Host "Latest winget release version already installed (ignore following settings error prefixing version number display `
     if settings not initialized by a previous run): $wingetCurrentVersion"
 }
 else {
-    # Install latest version of winget preview
+    # Install latest winget release version
     Push-Location $devbootPath\.winget\install
-    Start-BitsTransfer -Source https://aka.ms/getwingetpreview -Destination Microsoft.DesktopAppInstaller.msixbundle
+    $downloadUrl = (([System.Net.HttpWebRequest]::Create('https://github.com/microsoft/winget-cli/releases/latest/').GetResponse().ResponseUri.AbsoluteUri) -Replace 'tag', 'download') + '/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+    Start-BitsTransfer -Source $downloadUrl -Destination Microsoft.DesktopAppInstaller.msixbundle
     $wingetPreviewInstaller = Get-ChildItem -Filter Microsoft.DesktopAppInstaller*.msixbundle | Sort-Object LastWriteTime | Select-Object -Last 1 | Select-Object -ExpandProperty FullName
     Add-AppxPackage -Path $wingetPreviewInstaller
 }
 
-# Check/add 'configuration' experimental setting to winget settings file
-$wingetSettingsFilePath = "$env:LocalAppData\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\"
-$settingsFileName = "settings.json"
-$wingetSettingsFilePathFull = $wingetSettingsFilePath + $settingsFileName
-$wingetSettingsFile = Get-ChildItem -Path $env:LocalAppData\Packages\Microsoft.DesktopAppInstaller*\LocalState\settings.json | Select-Object -ExpandProperty FullName
-if ($null -eq $wingetSettingsFile) {
-    Write-Host "Could not find winget settings file; creating a new one"
-    # Create new empty winget settings file
-    New-Item -Path $wingetSettingsFilePath -Name $settingsFileName -ItemType File
-    $fileModel = New-Object -TypeName PSObject
-    $fileModel | Add-Member -MemberType NoteProperty -Name '$schema' -Value 'https://aka.ms/winget-settings.schema.json'
-    $fileModel | ConvertTo-Json | Set-Content $wingetSettingsFilePathFull
-    $wingetSettingsFile = Get-ChildItem -Path $wingetSettingsFilePathFull
-}
-$settings = Get-Content -Raw $wingetSettingsFile | ConvertFrom-Json
-$experimentalFeaturesSettings = $settings.experimentalFeatures
-if ($null -eq $experimentalFeaturesSettings) {
-    $settings | Add-Member -MemberType NoteProperty -Name experimentalFeatures -Value @{}
-    $settings.experimentalFeatures = @{'configuration'=$true}
-}
-else {
-    $experimentalFeatures = $settings.experimentalFeatures
-    if ($null -eq $experimentalFeatures.configuration) {
-        $experimentalFeatures | Add-Member -MemberType NoteProperty -Name configuration -Value $true
-    }
-    else {
-        if ($experimentalFeatures.configuration -eq $false) {
-            $experimentalFeatures.configuration = $true
-        }
-    }
-}
-$settings | ConvertTo-Json | Set-Content $wingetSettingsFile
+# # Deprecated by general release of WinGet Configurations without requiring enablement of experimental settings
+# # Check/add 'configuration' experimental setting to winget settings file
+# $wingetSettingsFilePath = "$env:LocalAppData\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\"
+# $settingsFileName = "settings.json"
+# $wingetSettingsFilePathFull = $wingetSettingsFilePath + $settingsFileName
+# $wingetSettingsFile = Get-ChildItem -Path $env:LocalAppData\Packages\Microsoft.DesktopAppInstaller*\LocalState\settings.json | Select-Object -ExpandProperty FullName
+# if ($null -eq $wingetSettingsFile) {
+#     Write-Host "Could not find winget settings file; creating a new one"
+#     # Create new empty winget settings file
+#     New-Item -Path $wingetSettingsFilePath -Name $settingsFileName -ItemType File
+#     $fileModel = New-Object -TypeName PSObject
+#     $fileModel | Add-Member -MemberType NoteProperty -Name '$schema' -Value 'https://aka.ms/winget-settings.schema.json'
+#     $fileModel | ConvertTo-Json | Set-Content $wingetSettingsFilePathFull
+#     $wingetSettingsFile = Get-ChildItem -Path $wingetSettingsFilePathFull
+# }
+# $settings = Get-Content -Raw $wingetSettingsFile | ConvertFrom-Json
+# $experimentalFeaturesSettings = $settings.experimentalFeatures
+# if ($null -eq $experimentalFeaturesSettings) {
+#     $settings | Add-Member -MemberType NoteProperty -Name experimentalFeatures -Value @{}
+#     $settings.experimentalFeatures = @{'configuration'=$true}
+# }
+# else {
+#     $experimentalFeatures = $settings.experimentalFeatures
+#     if ($null -eq $experimentalFeatures.configuration) {
+#         $experimentalFeatures | Add-Member -MemberType NoteProperty -Name configuration -Value $true
+#     }
+#     else {
+#         if ($experimentalFeatures.configuration -eq $false) {
+#             $experimentalFeatures.configuration = $true
+#         }
+#     }
+# }
+# $settings | ConvertTo-Json | Set-Content $wingetSettingsFile
 
 # Download winget configuration and dependencies (.vsconfig, etc.)
 Push-Location $devbootPath
